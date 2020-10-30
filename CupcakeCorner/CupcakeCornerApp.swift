44f886fcb8c4419ebd74a174dc46f7b35253e614
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-10-30 16:27:10 +0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by anurak teerarattananukulchai on 30/10/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
